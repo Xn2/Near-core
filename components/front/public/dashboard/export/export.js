3b68039ac1763663487b687cd6c78bea1520e43a
Xn2 <xini540@gmail.com>
2022-04-20 18:03:12 +0200

    Updated tachi export

--- components/front/public/dashboard/export/export.js
+++ components/front/public/dashboard/export/export.js
@@ -3,10 +3,7 @@ document.addEventListener('DOMContentLoaded', async function (e) {
 });
 
 async function getTachiExport(){
-  let maxVersion = 5
-  if (document.getElementById("noRemoveSV6").checked){
-    maxVersion = 6
-  }
+  let maxVersion = 6
   const json = await ( await fetch("/api/me/tachiExport", {method : "post", headers : {'Accept': 'application/json','Content-Type': 'application/json'}, body : JSON.stringify({maxVersion})})).json()
   const link = document.getElementById('exportLink')
   link.setAttribute('href', `data:text/plain;charset=utf-8, ${encodeURIComponent(JSON.stringify(json))}`)
