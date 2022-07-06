956b0d405f9d7960971b80c2f42f59af6056ecee
Marcus <xini540@gmail.com>
2022-07-06 14:56:45 +0200

    wtf

--- components/routes/api.js
+++ components/routes/api.js
@@ -7,7 +7,7 @@ const customSA = require('../../data/custom_SA.json')
 router.get("/", async(req, res) => { res.redirect('/web/login') })
 
 router.post('/api/register', passport.authenticate('register', {
-    successRedirect: '/api/code .ok',
+    successRedirect: '/api/ok',
     failureRedirect: '/api/nok'
 }))
 
