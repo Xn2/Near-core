b385d4eea24bdf1ac921c4ffdec8fda4934ac3d6
anjing-puteh <lowcl89@gmail.com>
2022-07-14 08:20:36 +0800

    Don't add self as rival
    Prevent user from adding themself as rival

--- components/routes/api.js
+++ components/routes/api.js
@@ -195,6 +195,7 @@ router.post('/api/me/addRival', async(req, res) => {
         if (user.rivals.length >= 3) { res.send("Rival limit reached."); return; }
         const rival = await db.User.findOne({ where: { friendCode: req.body.friendCode } })
         if (!rival) { res.send("Invalid friend code."); return; }
+        if (rival.friendCode == user.friendCode) { res.send("Cannot add self as rival."); return; }
         let rivals = user.rivals
         rivals.push({
             name: rival.ign,
@@ -206,6 +207,7 @@ router.post('/api/me/addRival', async(req, res) => {
     } else {
         const rival = await db.User.findOne({ where: { friendCode: req.body.friendCode } })
         if (!rival) { res.send("Invalid friend code."); return; }
+        if (rival.friendCode == user.friendCode) { res.send("Cannot add self as rival."); return; }
         let rivals = []
         rivals.push({
             name: rival.ign,
