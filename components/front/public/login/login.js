3403a994a7e3d10d939e41702b111f9525d32d84
Marcus REYNAUD <marcus.reynaud@devensys.com>
2022-07-19 18:16:25 +0200

    added auth error message in the frontend

--- components/front/public/login/login.js
+++ components/front/public/login/login.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('submitLogin').addEventListener('click', async (e) => {
+    document.getElementById('submitLogin').addEventListener('click', async(e) => {
+        document.getElementById('failure').setAttribute('hidden', 'true')
         const username = document.getElementById('username').value
-        const password = document.getElementById('password').value 
-        const postData = JSON.stringify({username, password})
-        const res = await fetch('/api/login', {method : "post" , headers: new Headers({'content-type': 'application/json'}), body : postData});
-        if (res.status == "200"){
+        const password = document.getElementById('password').value
+        const postData = JSON.stringify({ username, password })
+        const res = await fetch('/api/login', { method: "post", headers: new Headers({ 'content-type': 'application/json' }), body: postData });
+        if (res.status == "200") {
             window.location.href = '/web/dashboard'
+        } else {
+            document.getElementById('failure').removeAttribute('hidden')
         }
     })
 })
